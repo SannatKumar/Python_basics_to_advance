b9882e84bbd0edc4012d6f1bdee4809a8d024c61
Sannatkumar <interplaytheheads@gmail.com>
2019-06-04 11:54:35 +0900

    Printing types of the variables including complex

--- type.py
+++ type.py
@@ -11,3 +11,12 @@ print(type(a))
 print(type(b))
 
 # The type keyword is used to print the type of the assigned variable
+
+#complex numbers type as written as j as the imaginary part
+c = 3 + 5j
+d = 4j
+e = -5j
+
+print(type(c))
+print(type(d))
+print(type(e))
