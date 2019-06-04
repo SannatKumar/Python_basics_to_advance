47cb27194f9e19ba2360f308a0ae6f21286f0b7c
Sannatkumar <interplaytheheads@gmail.com>
2019-06-04 11:33:16 +0900

    Keywords to manipulate strings

---
+++ stringkeywords.py
@@ -0,0 +1,46 @@
+#simple string print
+print("Ram")
+
+#single quote can also be used
+print('Shyam')
+
+#string assignment
+a = "Narayana Vasudeva"
+print(a[2])# This prints third letter r counting 0,1,2
+
+#string position handling
+b = "Swayambhu Digambaraya"
+print(b[2:5])#prints third letter to fifth letter
+
+#removing whitespace
+c = " Hello, Universe! "
+print(c.strip())#removes spaces from front and end
+
+#length of the string
+d = "Iamlearningpython"
+print(len(d))
+
+# print lower case
+e = "HELLO WORLD"
+print(e.lower())
+
+#print upper case
+f = "upper case"
+print(f.upper())
+
+#replace method
+g = "Deed, Devotion"
+print(g.replace("D","N"))
+
+#split method
+h = "Split,me"
+print(h.split(","))
+
+
+
+
+
+
+
+
+
