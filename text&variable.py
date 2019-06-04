257d9de394f4c630fcc232e8687153ae2ebc05ac
Sannatkumar <interplaytheheads@gmail.com>
2019-06-04 11:44:26 +0900

    Assign text to variable and print output

--- text&variable.py
+++ text&variable.py
@@ -2,3 +2,12 @@
 
 txt = "variable works."
 print("This is how text and " + txt)# using + sign the output is presented
+
+# Other way is using arithmetic operators to add the variable
+
+pretext = "This is how text and "
+jointext = "variable works."
+completetext = pretext + jointext
+print(completetext)# This also gives the same result
+
+#Remember you cannot add variable and number. that is error
