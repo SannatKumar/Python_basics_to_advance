af82ead2dc35e579af151f614f0b3f02b9865acd
Sannatkumar <interplaytheheads@gmail.com>
2019-06-04 11:49:54 +0900

    Print the type of the variable

---
+++ type.py
@@ -0,0 +1,13 @@
+# Here we will print the type of the variable
+x = 1
+y = 2.45
+z = 35e45
+a = "mango"
+b = "type text"
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+
+# The type keyword is used to print the type of the assigned variable
