63984670d6b23842d57d1081f17c2c2fe78e5678
Sannatkumar <interplaytheheads@gmail.com>
2019-06-04 12:02:21 +0900

    inputstring.py

---
+++ inputstring.py
@@ -0,0 +1,17 @@
+# Here we will take string input from the user
+print("Enter your Name: ")
+Name = input()
+print("Hello, ", Name)
+
+#or
+print("Enter your Name: ")
+orName = input()
+print("Hello, " + orName)
+
+#both above method works
+
+# lets discuss unputting numbers also
+
+print("Enter your Number: ")
+Number = input()
+print("Its your Number: ", Number)
