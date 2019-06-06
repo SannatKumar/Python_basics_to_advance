19791bcf7a6c1705c83ec7c941887149dde80963
Sannatkumar <interplaytheheads@gmail.com>
2019-06-06 19:37:10 +0900

    Constructing list and appending values

---
+++ listconstructor.py
@@ -0,0 +1,7 @@
+#constructing list
+
+constructedlist = list(("Norway", "Finland", "France", "Denmark", "Sweden")) # this create the list name constructed list
+print(constructedlist)
+
+constructedlist.extend(["Germany"])
+print(constructedlist)
