f9e356cdfba853b76026d17b5874fb7c37bf0c8b
Sannatkumar <interplaytheheads@gmail.com>
2019-07-02 13:37:10 +0900

    Finding the categories list from reddit, from Tutorial

---
+++ webscrapping/categorieslist_redditscrape.py
@@ -0,0 +1,16 @@
+# Here is the tutorial to web scraping
+
+from bs4 import BeautifulSoup
+import requests
+
+url = requests.get('http://redditmetrics.com/top')
+
+soup = BeautifulSoup(url.text, 'html.parser')
+
+with open('sb.txt', 'w') as f:
+    for subreddit in soup.find_all('a'):
+        try:
+            if '/r' in subreddit.string:
+                f.write(subreddit.string[3:] + '\n')
+        except:
+            TypeError
